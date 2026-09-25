2012d235da40e7b22db5f1c761c7c7526105ad34
jboussin <jboussin@350028R-F115X16.lise.kreizenn.bzh>
2026-09-25 14:53:25 +0200

    exercice ligne 1,2

--- src/pkg00/helloworld/Helloworld.java
+++ src/pkg00/helloworld/Helloworld.java
@@ -3,17 +3,31 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package pkg00.helloworld;
+import java.util.Scanner;
 
-/**
- *
- * @author jboussin
- */
 public class Helloworld {
 
     public static void main(String[] args) {
-        System.out.println("ligne 1");
-        System.out.println("ligne 2");
+        //System.out.println("ligne 1");
+        //System.out.println("ligne 2");
+        //exo 4
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("x : ");
+        int x = sc.nextInt();
+        
+        System.out.print("y : ");
+        int y = sc.nextInt();
+        System.out.println("avant l'echange : x = " +x +" y: " +y );
+        
+        int z =0;
+        z=x;
+        x=y;
+        y=z;
+        
+        System.out.println("apres l'echange : x = " +x +" y: " +y );
+        
+                   
+    
     }
-    
-    
 }
